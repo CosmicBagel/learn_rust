2d05ac639f4b29e0726fbbba52eaada4e3f8dc4f
Samuel Whiteley <sam@cosmicbagel.com>
2021-03-27 16:13:44 -0600

    Added chp 9 error bits

---
+++ src/chp9.rs
@@ -0,0 +1,93 @@
+use std::{error::Error, fs::{self, File}, io::{self, ErrorKind, Read}};
+
+pub struct Guess {
+    value: i32,
+}
+
+impl Guess {
+    pub fn new(value: i32) -> Guess {
+        if value < 1 || value > 100 {
+            panic!("Guess value must be between 1 and 100, got {}.", value);
+        }
+
+        Guess { value }
+    }
+
+    pub fn value(&self) -> i32 {
+        self.value
+    }
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let guess: Guess = Guess::new("101".parse().unwrap());
+
+    let f = File::open("hello.txt")?;
+
+    Ok(())
+}
+
+fn read_username_from_file_most_elegant() -> Result<String, io::Error> {
+    fs::read_to_string("hello.txt")
+}
+
+fn read_username_from_file_more_elegant() -> Result<String, io::Error> {
+    let mut s = String::new();
+
+    File::open("hello.txt")?.read_to_string(&mut s)?;
+
+    Ok(s)
+}
+
+fn read_username_from_file_elegant() -> Result<String, io::Error> {
+    let mut f = File::open("hello.txt")?;
+    let mut s = String::new();
+    f.read_to_string(&mut s)?;
+
+    Ok(s)
+}
+
+fn read_username_from_file() -> Result<String, io::Error> {
+    let f = File::open("hello.txt");
+
+    let mut f = match f {
+        Ok(file) => file,
+        Err(e) => return Err(e),
+    };
+
+    let mut s = String::new();
+
+    match f.read_to_string(&mut s) {
+        Ok(_) => Ok(s),
+        Err(e) => Err(e),
+    }
+}
+
+fn fancy_file_open_with_closures() {
+    // mystical fancy file open
+    let f = File::open("hello.txt").unwrap_or_else(|error| {
+        if error.kind() == ErrorKind::NotFound {
+            File::create("hello.txt").unwrap_or_else(|error| {
+                panic!("Problem creating the file: {:?}", error);
+            })
+        } else {
+            panic!("Problem opening the file {:?}", error);
+        }
+    });
+}
+
+fn primitive_file_open() {
+    let f = File::open("hello.txt");
+
+    let f = match f {
+        Ok(file) => file,
+        Err(error) => match error.kind() {
+            ErrorKind::NotFound => match File::create("hello.txt") {
+                Ok(fc) => fc,
+                Err(e) => panic!("Problem creating the file: {:?}", e),
+            },
+            other_error => {
+                panic!("Failed to open file {:?}", other_error)
+            }
+        },
+    };
+}
