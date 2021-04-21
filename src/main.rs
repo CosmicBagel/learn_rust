bea76cb3061959846e741f160ef91d9ec9a7db32
Samuel Whiteley <sam@cosmicbagel.com>
2021-04-21 15:29:44 -0600

    moved minigrep code into its own files

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,3 @@
-use std::env;
-use std::process;
+fn main () {
 
-use alpine_cosmodrome::Config;
-
-static EXPECTED_USAGE: &str = "Expected usage: alpine_cosmodrome <search_query> <file_name>";
-
-fn main() {
-    let args: Vec<String> = env::args().collect();
-    let config = Config::new(&args).unwrap_or_else(|err| {
-        eprintln!("Failed to parse arguments: {}\n{}", err, EXPECTED_USAGE);
-        process::exit(1);
-    });
-
-    if let Err(e) = alpine_cosmodrome::run(config) {
-        eprintln!("Application error: {}", e);
-        process::exit(1);
-    }
 }
-
