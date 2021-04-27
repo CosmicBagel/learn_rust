99db4368ad6a1b022731cf316df202c37a0f4b06
Samuel Whiteley <sam@cosmicbagel.com>
2021-04-27 15:02:22 -0600

    Finished chp 16

---
+++ src/chp16-concurrency.rs
@@ -0,0 +1,120 @@
+use std::{sync::{Arc, Mutex, mpsc}, thread, time::Duration};
+
+fn main() {
+    {
+        let handle = thread::spawn(|| {
+            for i in 1..10 {
+                println!("hi number {} from the spawned thread", i);
+                thread::sleep(Duration::from_millis(1));
+            }
+        });
+
+        for i in 1..5 {
+            println!("hi number {} from the main thread", i);
+            thread::sleep(Duration::from_millis(1));
+        }
+
+        handle.join().unwrap();
+    }
+
+    //--------------------
+    {
+        let v = vec![1, 2, 3];
+
+        let handle = thread::spawn(move || {
+            println!("Here's a vector: {:?}", v);
+        });
+
+        // drop(v); //compiler error!
+
+        handle.join().unwrap();
+    }
+
+    //---------------------
+    {
+        let (tx, rx) = mpsc::channel();
+
+        thread::spawn(move || {
+            let val = String::from("Hi!");
+            tx.send(val).unwrap();
+            // println!("val is {}", val); //compiler error, we moved val into the channel
+        });
+
+        let received = rx.recv().unwrap();
+        println!("Got: {}", received);
+    }
+
+    //--------------------
+    {
+        let (tx, rx) = mpsc::channel();
+
+        let tx1 = tx.clone();
+        thread::spawn(move || {
+            let vals = vec![
+                String::from("hi"),
+                String::from("from"),
+                String::from("the"),
+                String::from("thread"),
+            ];
+
+            for val in vals {
+                tx1.send(val).unwrap();
+                thread::sleep(Duration::from_secs(1));
+            }
+            drop(tx1);
+
+            thread::sleep(Duration::from_secs(12));
+        });
+
+        thread::spawn(move || {
+            let vals = vec![
+                String::from("more"),
+                String::from("messages"),
+                String::from("for"),
+                String::from("you"),
+            ];
+
+            for val in vals {
+                tx.send(val).unwrap();
+                thread::sleep(Duration::from_secs(1));
+            }
+            drop(tx);
+
+            thread::sleep(Duration::from_secs(12));
+        });
+
+        for received in rx {
+            println!("Got: {}", received);
+        }
+    }
+
+    {
+        let m = Mutex::new(5);
+        {
+            let mut num = m.lock().unwrap();
+            *num = 6;
+        }
+
+        println!("m = {:?}", m);
+    }
+
+    {
+        let counter = Arc::new(Mutex::new(0));
+        let mut handles = vec![];
+
+        for _ in 0..10 {
+            let counter = Arc::clone(&counter);
+            let handle = thread::spawn(move || {
+                let mut num = counter.lock().unwrap();
+                *num += 1;
+            });
+            handles.push(handle);
+        }
+
+        for handle in handles {
+            handle.join().unwrap();
+        }
+
+        println!("Result: {}", *counter.lock().unwrap());
+    }
+}
